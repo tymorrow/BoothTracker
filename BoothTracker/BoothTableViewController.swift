337b2a1535fd0dd2f2210cf9f513305ded2787e8
Ty Morrow <tjm6f4@mst.edu>
2018-11-08 21:13:26 -0700

    Upgraded swift version; removed unit test projects; added icons.

--- BoothTracker/BoothTableViewController.swift
+++ BoothTracker/BoothTableViewController.swift
@@ -102,7 +102,7 @@ class BoothTableViewController: UITableViewController {
     }
 
     // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
             booths.remove(at: indexPath.row)
